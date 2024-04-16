2aed2e3b7f97f8efcef5666edb0bfa0b05b527ec
KPMGE <jesuskevin254@gmail.com>
2024-04-15 21:57:50 -0300

    :sparkles: create phones model and migrations

---
+++ app/models/phone.ts
@@ -0,0 +1,24 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import Client from './client.js'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+
+export default class Phone extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare number: string
+
+  @column()
+  declare clientId: number
+
+  @belongsTo(() => Client)
+  declare client: BelongsTo<typeof Client>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
---
+++ database/migrations/1713228074786_create_phones_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'phones'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+
+      table.string('number', 250).notNullable().unique()
+      table.integer('client_id').unsigned().references('id').inTable('clients').onDelete('cascade')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
