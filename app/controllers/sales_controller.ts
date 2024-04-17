de36a91a9e5a9abdb86ed7bea76558f20c37c029
KPMGE <jesuskevin254@gmail.com>
2024-04-17 20:32:14 -0300

    :sparkles: compute unitary and total price on crate sale

--- app/controllers/sales_controller.ts
+++ app/controllers/sales_controller.ts
@@ -1,3 +1,4 @@
+import Product from '#models/product'
 import Sale from '#models/sale'
 import { ResponseStatus, type HttpContext } from '@adonisjs/core/http'
 
@@ -5,10 +6,18 @@ export default class SalesController {
   async store({ request }: HttpContext) {
     const { clientId, productId, amount } = request.only(['clientId', 'productId', 'amount'])
 
+    const product = await Product.find(productId)
+    if (!product) return ResponseStatus.NotFound
+
+    const unitaryPrice = product.price
+    const totalPrice = unitaryPrice * amount
+
     await Sale.create({
-      clientId: clientId,
-      productId: productId,
-      amount: amount,
+      clientId,
+      productId,
+      amount,
+      totalPrice,
+      unitaryPrice,
     })
 
     return ResponseStatus.Created
