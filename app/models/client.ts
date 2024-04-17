6e852be4d939cebffafde7b96e444ad0f8ba3514
KPMGE <jesuskevin254@gmail.com>
2024-04-17 20:13:27 -0300

    :recycle: update client model with sales table

--- app/models/client.ts
+++ app/models/client.ts
@@ -1,9 +1,9 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, hasMany, hasOne, manyToMany } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany, hasOne } from '@adonisjs/lucid/orm'
 import Address from './address.js'
-import type { HasMany, HasOne, ManyToMany } from '@adonisjs/lucid/types/relations'
+import type { HasMany, HasOne } from '@adonisjs/lucid/types/relations'
 import Phone from './phone.js'
-import Product from './product.js'
+import Sale from './sale.js'
 
 export default class Client extends BaseModel {
   @column({ isPrimary: true })
@@ -21,8 +21,8 @@ export default class Client extends BaseModel {
   @hasOne(() => Address)
   declare address: HasOne<typeof Address>
 
-  @manyToMany(() => Product)
-  declare sales: ManyToMany<typeof Product>
+  @hasMany(() => Sale)
+  declare sales: HasMany<typeof Sale>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
