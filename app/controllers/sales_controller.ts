4c4a5511abf95b08ae945434508be055ea5e20be
KPMGE <jesuskevin254@gmail.com>
2024-04-17 20:13:11 -0300

    :recycle: update create sale route

--- app/controllers/sales_controller.ts
+++ app/controllers/sales_controller.ts
@@ -1,19 +1,15 @@
-import Client from '#models/client'
-import Product from '#models/product'
+import Sale from '#models/sale'
 import { ResponseStatus, type HttpContext } from '@adonisjs/core/http'
 
 export default class SalesController {
   async store({ request }: HttpContext) {
-    const { clientId, productId } = request.only(['clientId', 'productId'])
+    const { clientId, productId, amount } = request.only(['clientId', 'productId', 'amount'])
 
-    const client = await Client.find(clientId)
-    const product = await Product.find(productId)
-
-    if (!product || !client) {
-      return ResponseStatus.NotFound
-    }
-
-    await client.related('sales').save(product)
+    await Sale.create({
+      clientId: clientId,
+      productId: productId,
+      amount: amount,
+    })
 
     return ResponseStatus.Created
   }
