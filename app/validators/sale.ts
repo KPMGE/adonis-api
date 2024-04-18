44c42214ef1b4bf6e9cf300c40ef3c025a19161c
KPMGE <jesuskevin254@gmail.com>
2024-04-18 08:51:44 -0300

    :recycle: update create sale validator

--- app/validators/sale.ts
+++ app/validators/sale.ts
@@ -3,9 +3,7 @@ import vine from '@vinejs/vine'
 export const createSaleValidator = vine.compile(
   vine.object({
     amount: vine.number().min(0),
-    params: vine.object({
-      productId: vine.number().positive(),
-      clientId: vine.number().positive(),
-    }),
+    productId: vine.number().positive(),
+    clientId: vine.number().positive(),
   })
 )
