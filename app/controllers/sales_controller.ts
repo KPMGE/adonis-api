3b3a928ba9f3774963c1875e0ac45a465e0cd6f0
KPMGE <jesuskevin254@gmail.com>
2024-04-18 08:34:33 -0300

    :lock: handle product or client not found on sales route

--- app/controllers/sales_controller.ts
+++ app/controllers/sales_controller.ts
@@ -1,13 +1,20 @@
+import Client from '#models/client'
 import Product from '#models/product'
 import Sale from '#models/sale'
 import { ResponseStatus, type HttpContext } from '@adonisjs/core/http'
 
 export default class SalesController {
-  async store({ request }: HttpContext) {
+  async store({ request, response }: HttpContext) {
     const { clientId, productId, amount } = request.only(['clientId', 'productId', 'amount'])
 
-    const product = await Product.find(productId)
-    if (!product) return ResponseStatus.NotFound
+    const findClientPromise = Client.find(clientId)
+    const findProductPromise = Product.find(productId)
+    const [client, product] = await Promise.all([findClientPromise, findProductPromise])
+
+    if (!product || !client) {
+      response.abort({ message: 'client or product not found' }, ResponseStatus.NotFound)
+      return
+    }
 
     const unitaryPrice = product.price
     const totalPrice = unitaryPrice * amount
