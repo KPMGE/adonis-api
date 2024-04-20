55f1bc23f5ce412492009e34974823f55b27e4de
KPMGE <jesuskevin254@gmail.com>
2024-04-20 16:58:45 -0300

    :recycle: update exception handler

--- app/exceptions/handler.ts
+++ app/exceptions/handler.ts
@@ -1,27 +1,21 @@
 import app from '@adonisjs/core/services/app'
-import { HttpContext, ExceptionHandler } from '@adonisjs/core/http'
+import { HttpContext, ExceptionHandler, ResponseStatus } from '@adonisjs/core/http'
 
 export default class HttpExceptionHandler extends ExceptionHandler {
-  /**
-   * In debug mode, the exception handler will display verbose errors
-   * with pretty printed stack traces.
-   */
   protected debug = !app.inProduction
 
-  /**
-   * The method is used for handling errors and returning
-   * response to the client
-   */
   async handle(error: unknown, ctx: HttpContext) {
-    return super.handle(error, ctx)
+    if (error.code === 'ER_DUP_ENTRY') {
+      return ctx.response
+        .status(ResponseStatus.UnprocessableEntity)
+        .json({ errror: 'Invalid credentials' })
+    }
+
+    return ctx.response
+      .status(ResponseStatus.InternalServerError)
+      .json({ errror: 'Internal server error' })
   }
 
-  /**
-   * The method is used to report error to the logging service or
-   * the third party error monitoring service.
-   *
-   * @note You should not attempt to send a response from this method.
-   */
   async report(error: unknown, ctx: HttpContext) {
     return super.report(error, ctx)
   }
